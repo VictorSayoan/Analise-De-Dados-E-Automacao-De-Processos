7b90b4509506ca79bda3244adf0730ba0d8cb1c6
Victor Sayoan <victor.fernandes@ee.ufcg.edu.br>
2023-07-25 19:25:25 -0300

    commit02

--- Projeto_Atualizado_05-Indicadores_de_venda/index.py
+++ Projeto_Atualizado_05-Indicadores_de_venda/index.py
@@ -12,6 +12,7 @@ from dash_bootstrap_templates import ThemeSwitchAIO
 FONT_AWESOME = "https://use.fontawesome.com/releases/v5.10.2/css/all.css"
 
 app = dash.Dash(__name__, external_stylesheets=[FONT_AWESOME, dbc.themes.QUARTZ])
+app.config.suppress_callback_exceptions = True
 app.scripts.config.serve_lacally=True
 server=app.server
 
@@ -40,19 +41,20 @@ Dataset = pd.read_excel(r"E:\Documentos-Victor\Arquivos_GitHub\Analise-De-Dados-
 Database_Atualizado=Dataset.copy()
 Database_Atualizado.drop('Data', axis=1, inplace=True)
 
-# ======== Layout ========== #
+df_1=Database_Atualizado.groupby(['ID Loja'])['Quantidade'].sum().reset_index()
+df_1=df_1.sort_values(by='Quantidade', ascending=False)
 
-app.layout=dbc.Container([
+# ======== Layout ========== #
     # Primeira Linha:
+app.layout = dbc.Container([
     dbc.Row([
-        
         dbc.Col([ # Primeiro Card
             dbc.Card([
                 dbc.CardBody([
                     dbc.Row([
                         dbc.Col([
-                            html.Legend("Sales Analytics", style={'text-align':'center'}),
-                            html.Legend("Vs Analytics", style={'text-align':'center'})
+                            html.Legend("Sales Analytics", style={'text-align': 'center'}),
+                            html.Legend("Vs Analytics", style={'text-align': 'center'})
                         ], sm=8),
                         dbc.Col([
                             html.Div(
@@ -64,18 +66,42 @@ app.layout=dbc.Container([
                         dbc.Col([
                             html.Div(
                                 dbc.Button("Visit our GitHub: ", href="https://github.com/VictorSayoan", target="blank",
-                                           style={'font-family':'Roboto', 'font-size':'18px'}),
+                                           style={'font-family': 'Roboto', 'font-size': '18px'}),
                                 className="d-flex justify-content-center"
                             )
-                        ], style={"margin-top":'10px'}, align='center')
+                        ], style={"margin-top": '10px'}, align='center')
                     ])
                 ]),
             ], style=tab_card),
         ], sm=4, lg=3),
 
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    dbc.Row([
+                        dbc.Col([
+                            dbc.Card([
+                                dcc.Graph(
+                                    id='graph1',
+                                    figure=px.bar(df_1, x=df_1.index, y='Quantidade', labels={'Quantidade': 'Quantidade'},
+                                                  height=200).update_layout(template=dbc.themes.QUARTZ)
+                                    )
+                            ], style=tab_card)
+                        ], style={"margin-top": '10px'}, align='center')
+                    ]),
+                    dbc.Row([
+                        dbc.Col([
+                            dbc.Card([
+                                dcc.Graph(id='graph2', className='dbc', config=config_graph)
+                            ], style=tab_card)
+                        ], style={"margin-top": '10px'}, align='center')
+                    ])
+                ])
+            ], style=tab_card)
+        ], sm=12, lg=5)
+    ], class_name='g-2 my-auto', style={'margin-top': '7px'})
+], fluid=True, style={'height': '100vh'})
 
-    ], class_name='g-2 my-auto', style={'margin-top':'7px'})
-], fluid=True, style={'height':'100vh'})
 # ======== Callbacks ======== #
 
 
